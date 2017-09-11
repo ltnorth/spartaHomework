790549c721e7caf05ab97cb02e719021bd6d027e
Liam North <liam_north@outlook.com>
2017-09-11 16:53:15 +0100

    Added player move prompt and randome computer move

---
+++ w02d01/rock-paper-scissors/app.js
@@ -0,0 +1,30 @@
+// The player move is gathered and stored in a variable
+    // Prompt
+var player = prompt("Please make your move: (r)ock (p)aper (s)cissors");
+// The app generates a random computer move
+  // Math.random, Math.floor 
+// The computer move is stored in a variable
+var comp = Math.floor(Math.random()*3);
+
+switch (comp) {
+  case 0:
+
+}
+
+
+
+// Compare moves and see who has won, or if it's a draw
+  // If the moves are the same, it's a draw
+  // Rock beats scissors
+  // Scissors beat paper
+  // Paper beats rock
+
+// Tell the user the outcome of the game
+    // Alert
+
+// BONUS
+    // Update the scores for computer and player
+
+    // The first player to reach a score of 10 is the overall winner
+
+    // Be able to reset the game
