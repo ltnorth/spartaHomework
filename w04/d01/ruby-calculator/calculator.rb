dfaa537e3e5f936e7f9c81fb28642e437d7d7395
Liam North <liam_north@outlook.com>
2017-09-25 16:38:27 +0100

    Basic calculator working, however all numbers are floats

--- w04/d01/ruby-calculator/calculator.rb
+++ w04/d01/ruby-calculator/calculator.rb
@@ -19,3 +19,36 @@ puts "\n***CalcIt*** \n\n(b)asic, (a)dvanced"
 which_calc = gets.chomp
 # puts which_calc
 
+if which_calc == "b"
+	puts "Enter your first number:"
+	num1 = gets.chomp.to_f
+	puts "Enter your second number:"
+	num2 = gets.chomp.to_f
+	puts "Which operation would you like to use? (a)ddition, (s)ubtraction, (m)ultiplication, (d)ivision"
+	op = gets.chomp
+	case op
+		when "a"
+		  puts add(num1, num2)
+		when "s"
+		  puts subtract(num1, num2)
+		when "m"
+		  puts multiply(num1, num2)
+		when "d"
+		  puts divide(num1, num2)
+		else
+		  puts "Invalid entry - calculator shutting down..."
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
