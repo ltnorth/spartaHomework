fdd39decf83fa35bd13315eedd80f432a3f3f3f2
Liam North <liam_north@outlook.com>
2017-09-25 16:27:39 +0100

    Basic maths methods defined

--- w04/d01/ruby-calculator/calculator.rb
+++ w04/d01/ruby-calculator/calculator.rb
@@ -1,5 +1,21 @@
+def add(*numbers)
+	numbers.reduce(:+)
+end
+def subtract(*numbers)
+	numbers.reduce(:-)
+end
+def multiply(*numbers)
+	numbers.reduce(:*)
+end
+def divide(*numbers)
+	numbers.reduce(:/)
+end
+
+
+
+
+
 puts "\n***CalcIt*** \n\n(b)asic, (a)dvanced"
 which_calc = gets.chomp
-
 # puts which_calc
 
