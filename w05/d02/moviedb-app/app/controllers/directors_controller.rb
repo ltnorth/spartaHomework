600e7ad5df822b797bef3a1ecb6f686876e2cc3e
Liam North <liam_north@outlook.com>
2017-10-03 18:05:03 +0100

    A working web app

--- w05/d02/moviedb-app/app/controllers/directors_controller.rb
+++ w05/d02/moviedb-app/app/controllers/directors_controller.rb
@@ -1,4 +1,5 @@
 class DirectorsController < ApplicationController
+	before_action :set_director, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@directors = Director.all
@@ -8,18 +9,35 @@ class DirectorsController < ApplicationController
 	end
 
 	def new
+		@director = Director.new
 	end
 
 	def create
+		@director = Director.new(director_params)
+		@director.save
+		redirect_to directors_url
 	end
 
 	def edit
 	end
 
 	def update
+		@director.update(director_params)
+		redirect_to directors_url
 	end
 
-	def delete
+	def destroy
+		@director.destroy
+    redirect_to directors_url
 	end
+
+	private
+    def set_director
+      @director = Director.find(params[:id])
+    end
+
+    def director_params
+      params.require(:director).permit(:name, :dob, :rating)
+    end
 
 end
--- w05/d02/moviedb-app/app/controllers/movies_controller.rb
+++ w05/d02/moviedb-app/app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+	before_action :set_movie, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@movies = Movie.all
@@ -8,18 +9,35 @@ class MoviesController < ApplicationController
 	end
 
 	def new
+		@movie = Movie.new
 	end
 
 	def create
+		@movie = Movie.new(movie_params)
+		@movie.save
+		redirect_to movies_url
 	end
 
 	def edit
 	end
 
 	def update
+		@movie.update(movie_params)
+		redirect_to movies_url
 	end
 
-	def delete
+	def destroy
+		@movie.destroy
+    redirect_to movies_url
 	end
+
+	private
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
+
+    def movie_params
+      params.require(:movie).permit(:title, :summary, :release, :director_id)
+    end
 
 end
