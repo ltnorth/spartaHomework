7529b76b0c2d58a5cf41d00621c8ddda38b2fda4
Liam North <liam_north@outlook.com>
2017-10-04 10:33:51 +0100

    Home page now works, css made into scss but style isn't as planned

--- w05/d02/moviedb-app/config/routes.rb
+++ w05/d02/moviedb-app/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 	resources :movies
 	resources :directors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/home', to: 'home#index'
 end
