40bb773ba7ffb6e5ec9b6ee1f2ab0885ab82e6d2
Liam North <liam_north@outlook.com>
2017-10-30 20:44:46 +0000

    Exercism folder removed from repository

--- exercism/ruby/gigasecond/gigasecond.rb
+++
@@ -1,9 +0,0 @@
-class Gigasecond
-	def self.from arg		
-		arg + 1e9
-	end
-end
-
-module BookKeeping
-  VERSION = 6
-end
--- exercism/ruby/hello-world/hello_world.rb
+++
@@ -1,7 +0,0 @@
-class HelloWorld
-
-	def self.hello
-		"Hello, World!"
-	end
-	
-end
